ed8ff1d278942c4b5610636a041ef2a574838e4a
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2024-05-05 10:52:13 -0500

    Added initializer to Curve Points

--- lua/libraries/curvelib/curves/curve-point.lua
+++ lua/libraries/curvelib/curves/curve-point.lua
@@ -19,8 +19,18 @@ function metatable:HasRightHandle()
     return self.RightHandle and isvector( self.RightHandle )
 end
 
-function CurvePoint()
+-- Creates a new Curve Point with an optional set of handles
+---@param mainPointPos Vector The position of the main point. Must be in the range [0-1].
+---@param leftHandlePos Vector? The position of the left handle. X coordinate be in the range [0-1].
+---@param rightHandlePos Vector? The position of the right handle. X coordinate be in the range [0-1].
+---@return Curve.CurvePoint
+function CurvePoint( mainPointPos, leftHandlePos, rightHandlePos )
     local curvePoint = {}
     setmetatable( curvePoint, metatable )
+
+    curvePoint.MainPoint = mainPointPos
+    if leftHandlePos then curvePoint.LeftHandle = leftHandlePos end
+    if rightHandlePos then curvePoint.RightHandle = rightHandlePos end
+
     return curvePoint
 end
