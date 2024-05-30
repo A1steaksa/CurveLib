dfa16f57b61849d34e5205a213bf84b444228fca
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2024-05-30 16:08:05 -0500

    Added debug "Edit" button to the toolbar to load a curve from the local player's eye traced entity

--- lua/libraries/curvelib/editor/toolbar/panel.lua
+++ lua/libraries/curvelib/editor/toolbar/panel.lua
@@ -17,6 +17,27 @@ local PANEL = {}
 
 function PANEL:Init()
     self:SetWide( Defaults.Size.Width )
+
+    local editButton = vgui.Create( "DButton", self )
+    editButton:SetText( "Edit" )
+    editButton:Dock( LEFT )
+    editButton:DockMargin( 0, 0, 5, 0 )
+    editButton.DoClick = function()
+        local traceEntity = LocalPlayer():GetEyeTrace().Entity
+        
+        if traceEntity then
+                local lookedAtCurve = traceEntity.Curve
+
+            if lookedAtCurve then
+                self:GetEditorFrame():EditCurve( lookedAtCurve )
+            else
+                print( "No curve found" )
+            end
+        else
+            print( "No entity found" )
+        end
+    end
+
 end
 
 function PANEL:Paint( width, height )
