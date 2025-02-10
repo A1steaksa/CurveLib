24447816aafb5d2e88fbf68a58276ab90bd7526f
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2025-02-10 08:17:17 -0600

    Rename HandleDragData to MainHandleDragData

--- lua/libraries/curvelib/editor/graph/modules/handle-dragging.lua
+++ lua/libraries/curvelib/editor/graph/modules/handle-dragging.lua
@@ -3,30 +3,30 @@ require( "vguihotload" )
 ---@type CurveLib.Editor.Utils
 local curveUtils = include( "libraries/curvelib/editor/utils.lua" )
 
----@alias HandleDragData CurveLib.Editor.Graph.Panel.HandleDragData
+---@alias MainHandleDragData CurveLib.Editor.Graph.Panel.MainHandleDragData
 
--- Data about the current Handle drag operation
----@class CurveLib.Editor.Graph.Panel.HandleDragData
----@field BoundingX integer The X coordinate of the bounding box
----@field BoundingY integer The Y coordinate of the bounding box
----@field BoundingWidth integer The width of the bounding box
----@field BoundingHeight integer The height of the bounding box
----@field DragStartX integer The X coordinate of the mouse when the drag started
----@field DragStartY integer The Y coordinate of the mouse when the drag started
-local HandleDragData = {}
-HandleDragData.BoundingX = 0
-HandleDragData.BoundingY = 0
-HandleDragData.BoundingWidth = 0
-HandleDragData.BoundingHeight = 0
-HandleDragData.DragStartX = 0
-HandleDragData.DragStartY = 0
-HandleDragData.DragEndX = 0
-HandleDragData.DragEndY = 0
+-- Data about a drag operation containing potentially multiple Main Handles
+---@class CurveLib.Editor.Graph.Panel.MainHandleDragData
+---@field BoundingX integer The X coordinate of the bounding box containing the dragged Handles
+---@field BoundingY integer The Y coordinate of the bounding box containing the dragged Handles
+---@field BoundingWidth integer The width of the bounding box containing the dragged Handles
+---@field BoundingHeight integer The height of the bounding box containing the dragged Handles
+---@field DragStartX integer The X coordinate of the mouse inside the bounding box when the drag started
+---@field DragStartY integer The Y coordinate of the mouse inside the bounding box when the drag started
+local MainHandleDragData = {}
+MainHandleDragData.BoundingX = 0
+MainHandleDragData.BoundingY = 0
+MainHandleDragData.BoundingWidth = 0
+MainHandleDragData.BoundingHeight = 0
+MainHandleDragData.DragStartX = 0
+MainHandleDragData.DragStartY = 0
+MainHandleDragData.DragEndX = 0
+MainHandleDragData.DragEndY = 0
 
 ---@class CurveLib.Editor.Graph.Panel
 ---@field IsDraggingHandles boolean Whether the user is currently dragging the selected Handles.
 ---@field HandleUnderMouseDown? BaseHandle | MainHandle | SideHandle The Handle, if any, that the cursor was over when the left mouse button was pressed.
----@field HandleDragData HandleDragData
+---@field MainHandleDragData MainHandleDragData
 ---@field SiblingDistance number The distance between the currently-being-dragged Handle's sibling Handle and their Main Handle.  Used to maintain this distance when mirroring rotation.
 PANEL = PANEL
 
@@ -34,7 +34,7 @@ if not PANEL then error( "Failed to load HandleDragging module of CurveLib.Edito
 
 PANEL.IsDraggingHandles = false
 PANEL.SiblingDistance = 0
-PANEL.HandleDragData = HandleDragData
+PANEL.MainHandleDragData = MainHandleDragData
 
 function PANEL:HandleDragThink()
     -- Check if we've moved the mouse enough to count as a drag
@@ -52,9 +52,7 @@ function PANEL:HandleDragThink()
     self:UpdateDraggedHandles()
 end
 
-
-function PANEL:UpdateDraggedHandles()
-    local dragData = self.HandleDragData
+    local dragData = self.MainHandleDragData
     local mouseX, mouseY = self:CursorPos()
 
     -- The area the bounding box can move within
